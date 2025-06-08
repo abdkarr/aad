3f2fbad1fb5478f481fca86855a278d0d9157843
abdkarr <7543956+abdkarr@users.noreply.github.com>
2025-06-08 12:38:44 -0400

    style: format calc_agreement_mat function

--- aad/calc_agreement_mat.py
+++ aad/calc_agreement_mat.py
@@ -1,27 +1,35 @@
 import numpy as np
+import numpy.typing as npt
 
-def calc_agreement_mat(responses: np.ndarray):
-    n_annotators = responses.shape[0]
+from numba import njit
 
-    agreement_mat = np.zeros((n_annotators, n_annotators))
-    n_co_observed = np.zeros((n_annotators, n_annotators))
+@njit
+def calc_agreement_mat(response_mat: npt.NDArray) -> npt.NDArray:
+    n_workers = response_mat.shape[0]
+
+    agreement_mat = np.zeros((n_workers, n_workers))
+    n_co_observed = np.zeros((n_workers, n_workers))
 
     observed_tasks = []
-    for i in range(n_annotators):
-        observed_tasks.append(np.where(responses[i, :] > 0)[0])
+    for i in range(n_workers):
+        observed_tasks.append(np.where(response_mat[i, :] > 0)[0])
 
-    for i in range(n_annotators):
-        for j in range(i+1, n_annotators):
-            common_tasks = np.intersect1d(
+    for i in range(n_workers):
+        for j in range(i+1, n_workers):
+            co_observed = np.intersect1d(
                 observed_tasks[i], observed_tasks[j], assume_unique=True
             )
             
-            n_co_observed[i, j] = len(common_tasks)
+            n_co_observed[i, j] = len(co_observed)
             n_co_observed[j, i] = n_co_observed[i, j]
 
-            responses_i = responses[i, common_tasks]
-            responses_j = responses[j, common_tasks]
-            agreement_mat[i, j] = np.mean(responses_i == responses_j)
+            if len(co_observed) > 0:
+                responses_i = response_mat[i, co_observed]
+                responses_j = response_mat[j, co_observed]
+                agreement_mat[i, j] = np.mean(responses_i == responses_j)
+            else:
+                agreement_mat[i, j] = 0
+            
             agreement_mat[j, i] = agreement_mat[i, j]
 
     return agreement_mat, n_co_observed
